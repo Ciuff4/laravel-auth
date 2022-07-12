09a2ea374067ac6ab96e22729b421fc7efa79fcb
Ciuff4 <gasperini_cristina@libero.it>
2022-07-12 13:55:04 +0200

    add: pulsanti action+show tabella

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">Category</th>
+            <th scope="col">Tags</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ( $posts as $post )
+            <tr>
+                <th scope="row">{{$post->id}}</th>
+                <td>{{$post->title}}</td>
+                <td>{{$post->genre}}</td>
+                <td></td>
+                <td class="d-flex">
+                    <a href="#" class="btn btn-primary">SHOW</a>
+                    <a href="#" class="btn btn-warning">MODIFY</a>
+                    <form action="">
+                        <button>Delete</button>
+                    </form>
+                </td>
+              </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
